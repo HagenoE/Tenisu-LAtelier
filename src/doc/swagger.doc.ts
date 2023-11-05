81a71be837cf25e01b43ced1638b79c79c5ee662
HagenoE <edward.javaharlal@gmail.com>
2023-11-05 19:16:15 +0100

    fix deploy issus

--- src/doc/swagger.doc.ts
+++ src/doc/swagger.doc.ts
@@ -4,7 +4,7 @@ import { type Express } from 'express'
 import path from 'node:path'
 // const dirname = url.fileURLToPath(new URL('..', import.meta.url))
 const dirname = path.join(__dirname, '..')
-console.log(dirname)
+
 const options = {
   info: {
     version: '1.0.0',
